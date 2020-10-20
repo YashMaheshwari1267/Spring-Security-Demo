3e1e1ee393acd526dd63a8f0536a037f789f4db9
Yash Maheshwari <yashmaheshwari696@gmail.com>
2020-10-20 10:53:45 +0530

    init commit

---
+++ src/com/spring/security/config/DemoAppConfig.java
@@ -0,0 +1,10 @@
+package com.spring.security.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.spring.security")
+public class DemoAppConfig {
+
+}
